16c324d3e0055106d73cf204fe8a214013be6354
GGBondFFF <qq1451397768@gmail.com>
2026-05-09 11:34:52 +0800

    Initial Isaac Lab template for g0_robot_lab

---
+++ source/g0_robot_lab/g0_robot_lab/tasks/manager_based/g0_robot_lab/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-G0-Robot-Lab-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.g0_robot_lab_env_cfg:G0RobotLabEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
